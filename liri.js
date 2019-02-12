9bc57446340dbcfd09481b65579449b21feabf03
speiris <shehanpeiris@gmail.com>
2019-02-11 18:25:52 -0600

    created other base files per instructions

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
+var dotenv = require("dotenv").config();
+
 // Axios package will be used to make all the API calls
 var axios = require("axios");
-var spotify = require("node-spotify-api");
+var moment = require("moment");
+var keys = require("./keys.js");
+
+// var spotify = require("node-spotify-api");
+// var omdb = require("omdb");
+
 
