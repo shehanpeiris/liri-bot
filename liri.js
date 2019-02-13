f70110713f30a42f58fd0098b8c42e8f861b5910
speiris <shehanpeiris@gmail.com>
2019-02-12 23:55:19 -0600

    Spotify functionality added

--- liri.js
+++ liri.js
@@ -1,11 +1,35 @@
 var dotenv = require("dotenv").config();
 
-// Axios package will be used to make all the API calls
+// Call all the packages and APIs needed for this app
 var axios = require("axios");
-var moment = require("moment");
+// var moment = require("moment");
 var keys = require("./keys.js");
 
-// var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 // var omdb = require("omdb");
 
+// Create global variables to capture user input
+var method = process.argv[2];
+var userQuery = process.argv[3];
+
+
+var spotify = new Spotify(keys.spotify);
+
+function spotifySearchFunc() {
+    spotify.search({ 
+        type: 'track',
+        query: userQuery,
+        }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+            console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name + "\nSong Name: " + data.tracks.items[0].name + "\nPreview URL: " + data.tracks.items[0].preview_url + "\nAlbum: " + data.tracks.items[0].album.name + "\n-----------"); 
+    });
+};
+
+switch(method) {
+    case "spotify-this-song":
+        spotifySearchFunc();
+        break;
+};
 
